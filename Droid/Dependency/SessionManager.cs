942553e8d50325504929a8734e753c94a8714a8f
D2KXamarin <XamarinD2K@gmail.com>
2016-06-30 19:13:42 +0530

    Abhishek-create folder dependency for kill process

--- Droid/Dependency/SessionManager.cs
+++ Droid/Dependency/SessionManager.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using Xamarin.Forms;
+using Android.Content;
+using Android.Preferences;
+using CMA.Droid;
+using CMA;
 
+[assembly:Dependency(typeof(SessionManager))]
 namespace CMA.Droid
 {
-	public class SessionManager
+	/// <summary>
+	/// Session manager for Android platform
+	/// </summary>
+	public class SessionManager : ISessionManager
 	{
 		public SessionManager ()
 		{
 		}
+
+		#region ISessionManager implementation
+
+
+		public void Exit()
+		{
+			Android.OS.Process.KillProcess (Android.OS.Process.MyPid());
+		}
+		#endregion
 	}
 }
 
