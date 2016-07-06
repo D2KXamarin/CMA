14116b7a4e39b00d266ff6e79688ca71ff378435
XamarinD2k <pantarmj@gmail.com>
2016-07-06 14:15:27 +0530

    Mahesh - Local To Server

---
+++ CMA/View/DataSync/LocalToServer.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CMA
+{
+	public partial class LocalToServer : ContentPage
+	{
+		public LocalToServer ()
+		{
+			InitializeComponent ();
+
+			btnUpload.Clicked += async (object sender, EventArgs e) => {
+				LocalToServerDataSync localToServerDataSync = new LocalToServerDataSync ();
+				bool result =	await localToServerDataSync.Process ();
+				if (result)
+					await DisplayAlert ("", "Process Completed Successfully", "OK");
+				else
+					await DisplayAlert ("", "Error... Please try again", "OK");
+			};
+		}
+	}
+}
+
